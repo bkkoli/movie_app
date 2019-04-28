e9d2becea60a0e806ef997538034b2203547d528
류복기 <ryubokgi@gmail.com>
2019-04-28 15:05:54 +0900

    Updates

---
+++ precache-manifest.902ccb8598b4913ab727f6f5b84b86a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "16b7af4b522fe567c6f4",
+    "url": "/movie_app/static/js/main.c5188a87.chunk.js"
+  },
+  {
+    "revision": "7b84b94a0152dffaa438",
+    "url": "/movie_app/static/js/2.1d057864.chunk.js"
+  },
+  {
+    "revision": "16b7af4b522fe567c6f4",
+    "url": "/movie_app/static/css/main.120c8744.chunk.css"
+  },
+  {
+    "revision": "838c958975be25fbf3c39cc0c96713ce",
+    "url": "/movie_app/index.html"
+  }
+];
